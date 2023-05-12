beaba08307cf828e3722b84f43019bfa25d8186a
AliGhosn1 <alighosn2099@gmail.com>
2023-05-12 15:10:43 -0400

    setup RR with GH

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,11 +9,11 @@ import { NavContextProvider } from './contexts/navigation-bar.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <NavContextProvider>
         <App />
       </NavContextProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
